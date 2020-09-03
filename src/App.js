1172f3089678ef164543810aec6baf2257bb8157
Alejandra Ferreira <alejandra@MacBook-Pro-3.local>
2020-09-03 14:22:12 +0200

    Updating dependencies

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Procastinator app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>
+            This is the begining of the app
+          </h1>
+        </div>
+       
       </header>
     </div>
   );
